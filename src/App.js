39a1cc34f0212436a14b59fc2c4d352d825dbab2
murej <jmartinec@mu-m-jmartinec.fritz.box>
2016-11-20 21:15:14 +0100

    Create react-app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <p>Jure Martinec is a European designer and technologist, researching how digital might be shaping the world, often through code and interactive artifacts. He currently works at IDEO and is with Ansambel.</p>
+          <p>Occasionaly he collaborates on projects and develop highly functional prototypes. Get in touch and follow on Twitter, LinkedIn or GitHub.</p>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
